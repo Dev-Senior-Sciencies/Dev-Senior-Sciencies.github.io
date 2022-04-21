12e897098e066c61d69d3c65371becbf1245d460
Samuel Lucas <samuellucas24383@gmail.com>
2022-04-21 18:52:16 -0300

    new função mouseout

--- script.js
+++ script.js
@@ -2,5 +2,8 @@ let links = document.getElementsByClassName('links');
 
 links[0].addEventListener("mouseover", function onClick(e) {
   e.target.classList.add('active')
+});
+
+links[0].addEventListener("mouseout", function onClick(e) {
   e.target.classList.remove('active')
 });
